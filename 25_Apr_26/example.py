d1612974b29a3c9e4b6a45b974b62af1ae663b58
siddheshkadam1881 <siddheshkadam111@gmail.com>
2026-08-11 00:26:02 +0530

    changes added

--- 25_Apr_26/example.py
+++ 25_Apr_26/example.py
@@ -1,3 +1,5 @@
+# precheck here
+
 import os
 import numpy as np
 import pandas as pd
@@ -15,4 +17,34 @@ print(df.head())
 
 print(df.shape)
 
+#get all columns 
 print(df.columns)
+
+
+
+# get all columns repeat no.1
+print(df.duplicated().sum())
+
+# get all columns name and null count
+print(df.isnull().sum())
+
+# get all null counts dataframes
+print(df.isnull().sum().sum())
+
+print(df.info())
+
+
+for i in df.columns:
+    print("**********************************************")
+    print(i)
+    print(set(df[i].tolist()))
+    print("**********************************************")
+
+
+
+df['pcv'] = df['pcv'].replace(r'^\s*\?\s*$', np.nan, regex=True)
+
+print(df['pcv'].mode()[0])
+
+df['pcv'] = df['pcv'].apply(lambda x: '43' if pd.isna(x) else x)
+print(df['pcv'].tolist())
